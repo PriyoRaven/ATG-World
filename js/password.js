856e8cd1a9aec9229f3cc99dce4cc268bd2dda9a
Raven <priyadarsi02@gmail.com>
2023-10-14 10:36:32 +0530

    Signin page complete not need to do the signup

---
+++ js/password.js
@@ -0,0 +1,8 @@
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById("passwordField");
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+    } else {
+      passwordField.type = "password";
+    }
+  }
